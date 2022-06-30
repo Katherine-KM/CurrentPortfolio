8699d54f3e37c424727ac4edf1b5d828b80fb263
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-06-30 12:40:01 -0400

    navbar fix

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle /></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#skills' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><RiServiceLine /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsBook /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageAltDetail /></a>
     </nav>
