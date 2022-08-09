73e66b2638cc30273993f047067192a8e86368d4
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-08-09 10:28:00 -0400

    add link to h5 in contact section. add form connectivity to netlify

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,16 +15,14 @@ const Contact = () => {
             <h3>Thank you for checking out my work!</h3>
             <p> If you like what you see, I am open to software engineer positions, collabs, projects, and coffee chats! I look forward to speaking with you. </p>
             <div className="contact-icon-container">
-              <a href="https://www.linkedin.com/in/katherinemarcott/"><BsLinkedin className='contact-icon'/></a>
-              <h5> Connect on LinkedIn </h5>
-              <a href="https://twitter.com/CodingKatCodes"><BsTwitter className='contact-icon'/></a>
-              <h5> Follow me on Twitter </h5>
-              <a href="https://github.com/KatherineMarcott"><FaGithub className='contact-icon'/></a>
-              <h5> Check Out My GitHub </h5>
+              <a href="https://www.linkedin.com/in/katherinemarcott/"><BsLinkedin className='contact-icon'/><h5> Connect on LinkedIn </h5></a>
+              <a href="https://twitter.com/CodingKatCodes"> <BsTwitter className='contact-icon'/><h5> Follow me on Twitter </h5></a>
+              <a href="https://github.com/KatherineMarcott"> <FaGithub className='contact-icon'/> <h5> Check Out My GitHub </h5></a>
             </div>
           </div>
         </div>
-        <form id="contact-form-netlify" name="Contact" data-netlify='true'>
+        
+        <form id="contact-form-netlify" name="Contact" data-netlify='true' action="/contact-form-netlify" method="post">
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="text" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
